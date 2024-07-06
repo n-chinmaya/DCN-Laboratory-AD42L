68fdd83d9956fd8a83680286b209f265f8bb0054
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-07-06 15:41:16 +0530

    Update server.c

--- PAQ4- RPC/server.c
+++ PAQ4- RPC/server.c
@@ -5,14 +5,11 @@
  */
 
 #include "add.h"
-
 int *
 add_1_svc(numbers *argp, struct svc_req *rqstp)
 {
 	static int  result;
-
 	printf("add(%d,%d)\n",argp->a,argp->b);
 	result=argp->a+argp->b;
-
 	return &result;
 }

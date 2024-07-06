b7e3561f42869734a17e353546bc25cbf2ddac50
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-07-06 13:11:39 +0530

    Update Leaky_Bucket.c

--- PAQ3/Leaky_Bucket.c
+++ PAQ3/Leaky_Bucket.c
@@ -1,40 +1,35 @@
 #include<stdio.h>
 int main(){
-    int incoming, outgoing, buck_capacity, n, store = 0;
-    printf(&quot;Enter bucket capacity, outgoing rate and no of inputs: &quot;);
-    scanf(&quot;%d %d %d&quot;, &amp;buck_capacity, &amp;outgoing, &amp;n);
-    while (n != 0) { //loop over total number of inputs
-        printf(&quot;Enter the number of incoming packets: &quot;);
-        scanf(&quot;%d&quot;, &amp;incoming);
-        printf(&quot;Incoming packet size %d\n&quot;, incoming);
-    if((incoming-outgoing) &lt;= (buck_capacity-store)) //it is possible to send without dropping
-    {
-      int sent = outgoing&gt;=incoming?incoming:outgoing; //if incoming is more than outgoing, total
-      sent will be outgoing rest buff
-      if(sent &lt; outgoing &amp;&amp; store != 0)  //if incoming&lt;outgoing, we can add values from the store to be sent till op cap is reached
-      { 
-        int remaining = outgoing-sent;
-        while(remaining &gt; 0 &amp;&amp; store != 0) //keeps adding one to sent until we run out of op cap or nothing left in buff
-        {
-          remaining -= 1;
-          store -= 1;
-          sent += 1;
+    int incoming, outgoing, buck_capacity, n, store = 0;
+    printf("Enter bucket capacity, outgoing rate and no of inputs: ");
+    scanf("%d %d %d", &buck_capacity, &outgoing, &n);
+    while (n != 0) {
+        printf("Enter the number of incoming packets: ");
+        scanf("%d%", &incoming);
+        printf("Incoming packet size %d\n", incoming);
+        if((incoming-outgoing) <= (buck_capacity-store)){
+          int sent = outgoing >=incoming?incoming:outgoing;
+          if(sent < outgoing && store != 0)
+          { 
+            int remaining = outgoing-sent;
+            while(remaining > 0 && store != 0)
+            {
+              remaining -= 1;
+              store -= 1;
+              sent += 1;
+            }
+          }
+          printf("%d packets sent out\n", sent);
+          if(outgoing < incoming){
+            store = store + (incoming - outgoing);
+          }
         }
-      }
-      printf(&quot;%d packets sent out\n&quot;, sent);
-      if(outgoing&lt;incoming)
-      {
-        store = store + (incoming - outgoing);
-      }
+        else{
+          int dropped = (incoming-outgoing)-(buck_capacity-store);
+          printf("%d packets dropped\n", dropped);
+          store = buck_capacity;
+        }
+        printf("%d out of %d space used in the buffer\n", store, buck_capacity);
+            n--;
     }
-    else //packets need to be dropped
-    {
-      int dropped = (incoming-outgoing)-(buck_capacity-store); //excess packets are the ones that
-      after sending cant be accomodated in buff
-      printf(&quot;%d packets dropped\n&quot;, dropped);
-      store = buck_capacity;
-    }
-    printf(&quot;%d out of %d space used in the buffer\n&quot;, store, buck_capacity);
-            n--;
-    }
 }
